931002096cee8af27562896ea378d7abaebe7ef5
Molly Olson <molly.olson@vanderbilt.edu>
2017-11-06 11:44:59 -0600

    rewrote code for temp application

--- R/generate_standard_tables.R
+++ R/generate_standard_tables.R
@@ -52,7 +52,8 @@ generate_standard_tables <- function(
     out
   }
   # --- garbage_pvalue
-  garbage_pvalue <- function(x, test_method){ "NV: " %|% formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>" }
+  garbage_pvalue <- function(x, test_method){ "NV: " %|% formatp(x, digits = 3) %|% 
+      "<sup>" %|% test_method %|% "</sup>" }
   # --- empty_pvalue
   empty_pvalue <- function(x, test_method){""}
   # ---  count_fmt
@@ -94,7 +95,8 @@ generate_standard_tables <- function(
   formatp <- function (x, digits = 3, sig = 0.05, sig_marker = c("", "")) 
   {
     p <- ifelse(is.na(x), NA, ifelse(round(x, digits) < 1/(10^digits), 
-                                     "< " %|% sprintf("%4." %|% digits %|% "f", 1/(10^digits)), 
+                                     "< " %|% sprintf("%4." %|% 
+                                      digits %|% "f", 1/(10^digits)), 
                                      sprintf("%4." %|% digits %|% "f", x)))
     p <- ifelse(is.na(x), NA, ifelse(x < sig, sig_marker[1] %|% 
                                        p %|% sig_marker[2], p))
@@ -119,182 +121,306 @@ generate_standard_tables <- function(
     if(is.numeric(x)) return(ifelse(is.na(x),0,x))
   } 
   
+  y_in <- rlang::sym(y)
+  #y_in <- dt[[y]]
+  #y_text <- y
+  cat_entry <- function(
+    out
+    , x
+    , y = y_in
+    , dt = data
+    , xlab = NULL
+    , pvalue = TRUE
+    , fmt = "%1.0f (%s)%s"
+    , pvalue_fmt = function(x, test_method){
+      formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>"
+    }
+  ){
+   #browser()
+    y = y_in
+    d2 <- eval(substitute(dt[,.(x,y)]))
+    tbl <- table(d2[[1]], d2[[2]], useNA = "always")
+    #browser()
+    dimt <- dim(tbl)
+    M <- tbl[-dimt[1], -dimt[2]]
+    
+    dimm <- dimt-1
+    addout <- get_out(dimt[1]+1, 2 + dimm[2] + dimm[2] - 1 + dimm[2])
+    dima <- dim(addout)
+    addout[-c(1:2), 1] <- "@@" %|% dimnames(M)[[1]]
+    addout[-c(1:2), 1:dimm[2] + 2] <- formatpct(M, fmt)
+    addout[2, 1] <- if(is.null(xlab)){label(d2[[1]])}else{xlab}
+    addout[2, 2] <- "N (%)"
+    addout[1,1:dimm[2] + 2] <- dimnames(M)[[2]]
+    
+    miss <- formatpct(rbind(colSums(M),tbl[dimt[1],-dimt[2]]))
+    addout[1, (dima[2] - dimm[2]+1):dima[2]] <- "Missing: " %|% dimnames(M)[[2]]
+    addout[2, (dima[2] - dimm[2]+1):dima[2]] <- miss[2,]
+    
+    for(j in 2:dimm[2]){
+      M_compare <- M[,c(1,j)]
+      addout[1, dimt[2] + j] <- "p-value: " %|%
+        dimnames(M)[[2]][1] %|% " vs " %|% dimnames(M)[[2]][j]
+      if(sum(M_compare)==0 | !pvalue) next
+      E_compare <- rowSums(M_compare) %*% t(colSums(M_compare)) / sum(M_compare)
+      smallest_expected_cell <- min(E_compare)
+      
+      if(smallest_expected_cell >= 1 | sum(M_compare)>2000){
+        withCallingHandlers(cst <- chisq.test(M_compare, correct = FALSE), warning = chi_approx)
+        stat <- cst$statistic * (sum(M_compare) - 1)/sum(M_compare)
+        pval <- pchisq(stat, cst$parameter, lower.tail = FALSE)
+        test_method <- "EP"
+      }else{
+        pval <- fisher.test(M_compare)$p.value
+        test_method <- "FE"
+      }
+      
+      addout[2, dimt[2] + j] <- pvalue_fmt(pval, test_method)
+    }
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+  cat_entry_ <- function(
+    out
+    , x
+    , y = y
+    , dt = data
+    , xlab = NULL
+    , pvalue = TRUE
+    , fmt = "%1.0f (%s)%s"
+    , pvalue_fmt = function(x, test_method){
+      formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>"
+    }
+  ){
+    
+    d2 <- dt[, c(x,y), with = FALSE]
+    tbl <- table(d2[[1]], d2[[2]], useNA = "always")
+    dimt <- dim(tbl)
+    M <- tbl[-dimt[1], -dimt[2]]
+    dimm <- dimt-1
+    
+    addout <- get_out(dimt[1]+1, 2 + dimm[2] + dimm[2] - 1 + dimm[2])
+    dima <- dim(addout)
+    addout[-c(1:2), 1] <- "@@" %|% dimnames(M)[[1]]
+    addout[-c(1:2), 1:dimm[2] + 2] <- formatpct(M, fmt)
+    addout[2, 1] <- if(is.null(xlab)){label(d2[[1]])}else{xlab}
+    addout[2, 2] <- "N (%)"
+    addout[1,1:dimm[2] + 2] <- dimnames(M)[[2]]
+    
+    miss <- formatpct(rbind(colSums(M),tbl[dimt[1],-dimt[2]]))
+    addout[1, (dima[2] - dimm[2]+1):dima[2]] <- "Missing: " %|% dimnames(M)[[2]]
+    addout[2, (dima[2] - dimm[2]+1):dima[2]] <- miss[2,]
+    
+    for(j in 2:dimm[2]){
+      addout[1, dimt[2] + j] <- "p-value: " %|%
+        dimnames(M)[[2]][1] %|% " vs " %|% dimnames(M)[[2]][j]
+      M_compare <- M[,c(1,j)]
+      if(sum(M_compare)==0 | !pvalue) next
+      E_compare <- rowSums(M_compare) %*% t(colSums(M_compare)) / sum(M_compare)
+      smallest_expected_cell <- min(E_compare)
+      
+      if(smallest_expected_cell >= 1){
+        withCallingHandlers(cst <- chisq.test(M_compare, correct = FALSE), 
+                            warning = chi_approx)
+        stat <- cst$statistic * (sum(M_compare) - 1)/sum(M_compare)
+        pval <- pchisq(stat, cst$parameter, lower.tail = FALSE)
+        test_method <- "EP"
+      }else{
+        pval <- fisher.test(M_compare)$p.value
+        test_method <- "FE"
+      }
+      addout[2, dimt[2] + j] <- pvalue_fmt(pval, test_method)
+    }
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+  n_unique <- function(out, x, y = y_in, dt = data, xlab = NULL){
+    y = y_in
+    #browser()
+    dt1 <- eval(substitute(dt[,.(unique(x), N = "N"), y][,table(N,y, useNA = "always")]))
+    dimt <- dim(dt1)
+    M <- dt1[-dimt[1],-dimt[2], drop = FALSE]
+    dimm <- dim(M)
+    
+    addout <- get_out(2, 2 + dimm[2] + dimm[2] - 1 + dimm[2])
+    dima <- dim(addout)
+    addout[1,1:dimm[2] + 2] <- dimnames(M)[[2]]
+    addout[2,1:dimm[2] + 2] <- M[1,]
+    addout[2,1] <- if(!is.null(xlab)){xlab}else{eval(substitute(label(dt[,.(x)][[1]])))}
+    
+    addout[1, (dima[2] - dimm[2] + 1):dima[2]] <- "Missing: " %|% dimnames(M)[[2]]
+    addout[2, (dima[2] - dimm[2] + 1):dima[2]] <- dt1[dimt[1],-dimt[2]]
+    addout[2,2] <- "N"
+    
+    for(j in 2:dimm[2]){
+      addout[1, 2 + dimm[2] + j - 1] <- "p-value: " %|%
+        addout[1, 2 + 1] %|% " vs " %|%  addout[1, 2 + j]
+    }
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+
+  
+  
+  cont_entry <- function(
+    out
+    , x
+    , y = y_in
+    , dt = data
+    , xlab = NULL
+    , pvalue_fmt = function(x, test_method){
+      formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>"
+    }
+  ){
+    y = y_in
+  #  browser()
+    d1 <- eval(substitute(dt[,.(x,y)])) ## mao: changed data[,.(x,y)] to dt[.(x,y)]
+    d2 <- d1[complete.cases(d1)]
+    d3 <- eval(substitute(
+      d2[,.(N = .N, Mean = mean(x), SD = sd(x), Q1 = as.numeric(quantile(x, .25)),
+            Median = as.numeric(median(x)), Q3 = as.numeric(quantile(x, .75))), y] %>%
+        arrange(y)
+    ))
+    
+    dimt <- dim(d3)
+    addout <- get_out(dimt[2] + 1, 2 + dimt[1] + dimt[1] - 1 + dimt[1])
+    dima <- dim(addout)
+    addout[1,1:dimt[1] + 2] <- d3[[1]]
+    addout[1:(dimt[2]-1) + 2, 1:dimt[1] + 2] <- round(t(as.matrix(d3[, -1])))
+    addout[2,1] <- if(!is.null(xlab)){xlab}else{eval(substitute(label(dt[,.(x)][[1]])))}
+    
+    miss <- table(factor(1*is.na(d1[[1]]), 0:1, 0:1),d1[[2]])
+    addout[1,(dima[2] - dimt[1] + 1):(dima[2])] <- "Missing: " %|% dimnames(miss)[[2]]
+    addout[2,(dima[2] - dimt[1] + 1):(dima[2])] <- formatpct(miss)[2,]
+    
+    addout[1:(dimt[2]-1) + 2, 2] <- names(d3)[-1]
+    
+    for(j in 2:dimt[1]){
+      holdin <- d3[[1]][c(1,j)]
+      d4 <- eval(substitute(d2 %>% filter(y %in% holdin)))
+      wt1 <- eval(substitute(wilcox.test(x ~ y, data = d4)))
+      addout[2, 2 + dimt[1] + j - 1] <- pvalue_fmt(wt1$p.value, test_method = "WR")
+      addout[1, 2 + dimt[1] + j - 1] <- "p-value: " %|%
+        addout[1, 2 + 1] %|% " vs " %|%  addout[1, 2 + j]
+    }
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+  
+  binary_entry <- function(
+    out
+    , x
+    , y = y_in
+    , dt = data
+    , xlab = NULL
+    , level = c("Yes", "1")
+    , pvalue = TRUE
+    , fmt = "%1.0f (%s)%s"
+    , pvalue_fmt = function(x, test_method){
+      formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>"
+    }
+  ){
+    y = y_in
+   # browser()
+    cat <- eval(substitute(cat_entry(
+      list()
+      , x
+      , y
+      , dt
+      , xlab
+      , pvalue = pvalue
+      , fmt = fmt
+      , pvalue_fmt = pvalue_fmt
+    )))
+    
+    ny <- length(eval(substitute(dt[,table(y)])))
+    count_cols <- 1:ny + 2
+    matches <- cat[[1]][-c(1:2),1] %in% c("@@" %|% level)
+    row <- if(sum(matches)==1){ which(matches) }else{1}
+    cat[[1]][2,count_cols] <- cat[[1]][2 + row, count_cols]
+    addout <- cat[[1]][1:2,]
+    addout[2,2] <- ""
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+  binary_entry_ <- function(
+    out
+    , x
+    , y = y
+    , dt = data
+    , xlab = NULL
+    , level = c("Yes", "1")
+    , pvalue = TRUE
+    , fmt = "%1.0f (%s)%s"
+    , pvalue_fmt = function(x, test_method){
+      formatp(x, digits = 3) %|% "<sup>" %|% test_method %|% "</sup>"
+    }
+  ){
+    
+    cat <- cat_entry_(list(),x,y,dt,xlab,pvalue = pvalue, fmt = fmt, pvalue_fmt = pvalue_fmt)
+    ny <- length(unique(dt[[y]]))
+    count_cols <- 1:ny + 2
+    matches <- cat[[1]][-c(1:2),1] %in% c("@@" %|% level)
+    row <- if(sum(matches)==1){ which(matches) }else{1}
+    cat[[1]][2,count_cols] <- cat[[1]][2 + row, count_cols]
+    addout <- cat[[1]][1:2,]
+    addout[2,2] <- ""
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+  empty_entry <- function(
+    out
+    , y = y_in
+    , dt = data
+    , fill = ""){
+    y = y_in
+    d2 <- eval(substitute(dt[,.(j = 1, y)]))
+    label(d2[[1]]) <- "J"
+    d2[[1]][1] <- 0
+    addout <- eval(substitute(cat_entry(list(),j,y, d2)))[[1]][1:2, ]
+    addout[2,] <- ""
+    for(j in 1:min(length(fill),ncol(addout))) addout[2, j] <- fill[j]
+    
+    if(length(out)>0) addout <- addout[-1,]
+    out[[length(out)+1]] <- addout
+    return(out)
+  }
+  
+
+  
+  
   ##########################################
   ## start function coding here
   if(missing(y)) stop("Provide a y variable")
   if(missing(data)) stop("Provide a data table")
   if(is.character(dt)) stop("data should be a data table, not a character string")
   
-  dt <- deparse(substitute(data))
+  #dt <- deparse(substitute(data))
 
-  #########################################
-  ## get "new" functions based on new y, etc
-  fn1 <- paste("cat_entry <- function("
-  ,"out"
-  ,", x"
-  ,", y  "
-  ,", dt "
-  , ", xlab = NULL"
-  , ", pvalue = TRUE"
-  ,", fmt = \"%1.0f (%s)%s\""
-  ,", pvalue_fmt = function(x, test_method){"
-  ,"formatp(x, digits = 3) %|% \"<sup>\" %|% test_method %|% \"</sup>\""
-  ,"}"
-  ,"){"
-  ,"d2 <- eval(substitute(dt[,.(x,y)]))"
-  ,"tbl <- table(d2[[1]], d2[[2]], useNA = \"always\")"
-  ,"dimt <- dim(tbl)"
-  ,"M <- tbl[-dimt[1], -dimt[2]]"
-  ,"dimm <- dimt-1"
-  ,"addout <- get_out(dimt[1]+1, 2 + dimm[2] + dimm[2] - 1 + dimm[2])"
-  ,"dima <- dim(addout)"
-  ,"addout[-c(1:2), 1] <- \"@@\" %|% dimnames(M)[[1]]"
-  ,"addout[-c(1:2), 1:dimm[2] + 2] <- formatpct(M, fmt)"
-  ,"addout[2, 1] <- if(is.null(xlab)){label(d2[[1]])}else{xlab}"
-  ,"addout[2, 2] <- \"N (%)\""
-  ,"addout[1,1:dimm[2] + 2] <- dimnames(M)[[2]]"
-  ,"miss <- formatpct(rbind(colSums(M),tbl[dimt[1],-dimt[2]]))"
-  ,"addout[1, (dima[2] - dimm[2]+1):dima[2]] <- \"Missing: \" %|% dimnames(M)[[2]]"
-  ,"addout[2, (dima[2] - dimm[2]+1):dima[2]] <- miss[2,]"
-  ,"for(j in 2:dimm[2]){"
-  ,"M_compare <- M[,c(1,j)]"
-  ,"addout[1, dimt[2] + j] <- \"p-value: \" %|%"
-  ,"dimnames(M)[[2]][1] %|% \" vs \" %|% dimnames(M)[[2]][j]"
-  ,"if(sum(M_compare)==0 | !pvalue) next"
-  ,"E_compare <- rowSums(M_compare) %*% t(colSums(M_compare)) / sum(M_compare)"
-  ,"smallest_expected_cell <- min(E_compare)"
-  ,"if(smallest_expected_cell >= 1 | sum(M_compare)>2000){"
-  ,"withCallingHandlers(cst <- chisq.test(M_compare, correct = FALSE), warning = chi_approx)"
-  ,"stat <- cst$statistic * (sum(M_compare) - 1)/sum(M_compare)"
-  ,"pval <- pchisq(stat, cst$parameter, lower.tail = FALSE)"
-  ,"test_method <- \"EP\""
-  ,"}else{"
-  ,"pval <- fisher.test(M_compare)$p.value"
-  ,"test_method <- \"FE\""
-  ,"}"
-  ,"addout[2, dimt[2] + j] <- pvalue_fmt(pval, test_method)"
-  ,"}"
-  ,"if(length(out)>0) addout <- addout[-1,]"
-  ,"out[[length(out)+1]] <- addout"
-  ,"return(out)"
-  ,"}"
-  ,"n_unique <- function(out"
-  ,", x"
-  ,", y "
-  ,", dt "
-  ,", xlab = NULL){"
-  ,"dt1 <- eval(substitute(dt[,.(unique(x), N = \"N\"), y][,table(N,y, useNA = \"always\")]))"
-  ,"dimt <- dim(dt1)"
-  ,"M <- dt1[-dimt[1],-dimt[2], drop = FALSE]"
-  ,"dimm <- dim(M)"
-  ,"addout <- get_out(2, 2 + dimm[2] + dimm[2] - 1 + dimm[2])"
-  ,"dima <- dim(addout)"
-  ,"addout[1,1:dimm[2] + 2] <- dimnames(M)[[2]]"
-  ,"addout[2,1:dimm[2] + 2] <- M[1,]"
-  ,"addout[2,1] <- if(!is.null(xlab)){xlab}else{eval(substitute(label(dt[,.(x)][[1]])))}"
-  ,"addout[1, (dima[2] - dimm[2] + 1):dima[2]] <- \"Missing: \" %|% dimnames(M)[[2]]"
-  ,"addout[2, (dima[2] - dimm[2] + 1):dima[2]] <- dt1[dimt[1],-dimt[2]]"
-  ,"addout[2,2] <- \"N\""
-  ,"for(j in 2:dimm[2]){"
-  ,"addout[1, 2 + dimm[2] + j - 1] <- \"p-value: \" %|%"
-  ,"addout[1, 2 + 1] %|% \" vs \" %|%  addout[1, 2 + j]"
-  ,"}"
-  ,"if(length(out)>0) addout <- addout[-1,]"
-  ,"out[[length(out)+1]] <- addout"
-  ,"return(out)"
-  ,"}"
-  ,"cont_entry <- function("
-  ,"out"
-  ,",x"
-  ,", y "
-  ,", dt "
-  ,", xlab = NULL"
-  ,", pvalue_fmt = function(x, test_method){"
-  ,"formatp(x, digits = 3) %|% \"<sup>\" %|% test_method %|% \"</sup>\""
-  ,"}"
-  ,"){"
-  ,"d1 <- eval(substitute(dt[,.(x,y)])) ## mao: changed data[,.(x,y)] to dt[.(x,y)]"
-  ,"d2 <- d1[complete.cases(d1)]"
-  ,"d3 <- eval(substitute("
-  ,"d2[,.(N = .N, Mean = mean(x), SD = sd(x), Q1 = as.numeric(quantile(x, .25)), Median = as.numeric(median(x)), Q3 = as.numeric(quantile(x, .75))), y] %>%"
-  ,"arrange(y)"
-  ,"))"
-  ,"dimt <- dim(d3)"
-  ,"addout <- get_out(dimt[2] + 1, 2 + dimt[1] + dimt[1] - 1 + dimt[1])"
-  ,"dima <- dim(addout)"
-  ,"addout[1,1:dimt[1] + 2] <- d3[[1]]"
-  ,"addout[1:(dimt[2]-1) + 2, 1:dimt[1] + 2] <- round(t(as.matrix(d3[, -1])))"
-  ,"addout[2,1] <- if(!is.null(xlab)){xlab}else{eval(substitute(label(dt[,.(x)][[1]])))}"
-  ,"miss <- table(factor(1*is.na(d1[[1]]), 0:1, 0:1),d1[[2]])"
-  ,"addout[1,(dima[2] - dimt[1] + 1):(dima[2])] <- \"Missing: \" %|% dimnames(miss)[[2]]"
-  ,"addout[2,(dima[2] - dimt[1] + 1):(dima[2])] <- formatpct(miss)[2,]"
-  ,"addout[1:(dimt[2]-1) + 2, 2] <- names(d3)[-1]"
-  ,"for(j in 2:dimt[1]){"
-  ,"holdin <- d3[[1]][c(1,j)]"
-  ,"d4 <- eval(substitute(d2 %>% filter(y %in% holdin)))"
-  ,"wt1 <- eval(substitute(wilcox.test(x ~ y, data = d4)))"
-  ,"addout[2, 2 + dimt[1] + j - 1] <- pvalue_fmt(wt1$p.value, test_method = \"WR\")"
-  ,"addout[1, 2 + dimt[1] + j - 1] <- \"p-value: \" %|%"
-  ,"addout[1, 2 + 1] %|% \" vs \" %|%  addout[1, 2 + j]"
-  ,"}"
-  ,"if(length(out)>0) addout <- addout[-1,]"
-  ,"out[[length(out)+1]] <- addout"
-  ,"return(out)"
-  ,"}"
-  ,"binary_entry <- function("
-  ,"out"
-  ,", x "
-  ,", y "
-  ,", dt "
-  ,", xlab = NULL"
-  ,", level = c(\"Yes\", \"1\")"
-  ,", pvalue = TRUE"
-  ,", fmt = \"%1.0f (%s)%s\""
-  ,", pvalue_fmt = function(x, test_method){"
-  ,"formatp(x, digits = 3) %|% \"<sup>\" %|% test_method %|% \"</sup>\""
-  ,"}"
-  ,"){"
-  ,"cat <- eval(substitute(cat_entry("
-  ,"list()"
-  ,", x "
-  ,", y "
-  ,", dt "
-  ,", xlab "
-  ,", pvalue = pvalue"
-  ,", fmt = fmt"
-  ,", pvalue_fmt = pvalue_fmt"
-  ,")))"
-  ,"ny <- eval(substitute(dt[,length(unique(y))]))"
-  ,"count_cols <- 1:ny + 2"
-  ,"matches <- cat[[1]][-c(1:2),1] %in% c(\"@@\" %|% level)"
-  ,"row <- if(sum(matches)==1){ which(matches) }else{1}"
-  ,"cat[[1]][2,count_cols] <- cat[[1]][2 + row, count_cols]"
-  ,"addout <- cat[[1]][1:2,]"
-  ,"addout[2,2] <- \"\""
-  ,"if(length(out)>0) addout <- addout[-1,]"
-  ,"out[[length(out)+1]] <- addout"
-  ,"return(out)"
-  ,"}"
-  ,"empty_entry <- function(out "
-  ,", y "
-  ,", dt "
-  ,", fill = \"\"){"
-  ,"d2 <- eval(substitute(dt[,.(j = 1, y)]))"
-  ,"label(d2[[1]]) <- \"\""
-  ,"d2[[1]][1] <- 0"
-  ,"addout <- eval(substitute(cat_entry(list(),j,y, d2)))[[1]][1:2, ]"
-  ,"addout[2,] <- \"\""
-  ,"for(j in 1:min(length(fill),ncol(addout))) addout[2, j] <- fill[j]"
-  ,"if(length(out)>0) addout <- addout[-1,]"
-  ,"out[[length(out)+1]] <- addout"
-  ,"return(out)"
-  ,"} "
-)
-  ## individualize functions for specific y and specific dt
-  fn2 <- gsub(", y ", paste0(", y = ", y) ,fn1)
-  fn3 <- gsub(", dt ", paste0(", dt = ", dt), fn2)
-  
-  eval(parse(text = fn3))
+#browser()
+  # ## individualize functions for specific y and specific dt
+  # fn2 <- gsub(", y ", paste0(", y = ", y) ,fn1)
+  # fn3 <- gsub(", dt ", paste0(", dt = ", dt), fn2)
+  # 
+  # eval(parse(text = fn3))
   #####################################################
   
   if(changes == TRUE){
@@ -317,7 +443,7 @@ generate_standard_tables <- function(
     
     #assign(noquote("pop" %|% pop %|% "tbl" %|% tbl),tbl1)
     cat("\n### Table " %|% tbl %|% ": " %|% attr(tbln, "title") %|% "\n\n")
-    tbln <- tbln[,1:(length(unique(eval(parse(text = dt))[[y]]))+2)]
+    tbln <- tbln[,1:(length(unique(data[[y]]))+2)]
     names(tbln) <- tbln[1,]
     tbln[,1] <- gsub("@@","&nbsp;&nbsp;&nbsp;", tbln[,1])
     
