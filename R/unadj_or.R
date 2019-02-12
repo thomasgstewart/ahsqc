6409dbf1e27021a214b79eb8fe2a6a0daeef100e
Molly Olson <molly.olson@vanderbilt.edu>
2019-02-12 13:23:54 -0600

    unadj_or

---
+++ R/unadj_or.R
@@ -0,0 +1,28 @@
+#' Returns odds ratio of two variables from data frame
+#'
+#' @details Returns a dataframe with variable, estimate, and CI of odds ratio
+#' @param v1 Character, variable 1 name
+#' @param v2 Character, variable 2 name
+#' @param data dataframe or datatable
+#' @keywords AHSQC, odds ratio, OR
+#' @export
+#' @examples
+#' # Not run:
+#' # unadj_or("flg_cmp_postop_ssi", "e_gender", data = d1)
+
+
+unadj_or <- function(v1, v2, d1){
+  require(epitools)
+  or <- oddsratio(table(d1[[v1]], d1[[v2]]) %>% as.matrix)$measure[2,]
+  out <- data.frame(
+    var = label(d1[[v2]]),
+    est = or[1],
+    lb = or[2],
+    ub = or[3],
+    ci = signif(or[1],3)  %|% " (" %|% signif(or[2], 3) %|% ", " %|% signif(or[3], 3) %|% ")",
+    N = sum(!is.na(d1[[v2]])),
+    stringsAsFactors = FALSE,
+    row.names = NULL
+  )
+  return(out)
+}
